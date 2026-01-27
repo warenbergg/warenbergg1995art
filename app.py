ce9fa645a2a113faba40f3308c258bf2b59dd738
warenbergg <warenbergg99@gmail.com>
2026-01-27 22:32:46 +0800

    move videos to cloudinary

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import os
 app = Flask(__name__)
 
 videos = [
-    "videos/art009.mp4",
-    "videos/art014.mp4",
-    "videos/art0010.mp4",
-    "videos/art0006.mp4",
-    "videos/art003.mp4"
+    "https://res.cloudinary.com/dru9omgli/video/upload/v1769524131/art014_yisenx.mp4",
+    "https://res.cloudinary.com/dru9omgli/video/upload/v1769524095/art0006_v6ylxa.mp4",
+    "https://res.cloudinary.com/dru9omgli/video/upload/v1769524090/art009_hjabtl.mp4",
+    "https://res.cloudinary.com/dru9omgli/video/upload/v1769524085/art0010_wixnse.mp4",
+    "https://res.cloudinary.com/dru9omgli/video/upload/v1769524040/art003_zn5tpf.mp4"
 ]
 
 @app.route("/")
 def home():
     data = [url_for("static", filename=v) for v in videos]
-    return render_template("index.html", videos=data)
+    return render_template("index.html", videos=videos)
 
 #about
 @app.route("/about")
